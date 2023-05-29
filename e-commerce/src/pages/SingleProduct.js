9e54e2fa9ea7ab547ffedde6f3224b03d722f4a3
mutuku1998 <martinmutuku1998@gmail.com>
2023-05-29 20:11:53 +0300

    progressing

--- e-commerce/src/pages/SingleProduct.js
+++ e-commerce/src/pages/SingleProduct.js
@@ -3,8 +3,10 @@ import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
 import ProductCard from '../components/ProductCard';
 import ReactStars from "react-rating-stars-component";
+import ReactImageZoom from 'react-image-zoom';
 import { useState } from 'react';
 const SingleProduct = () => {
+  const props = {width: 400, height: 500, zoomWidth: 500, img: "https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/63/196649/1.jpg?9259"};
   const [orderedProduct, setorderedProduct] = useState(true);
   return (
     <>
@@ -13,8 +15,38 @@ const SingleProduct = () => {
        <div className='main-product-wrapper py-5 home-wrapper-2'>
         <div className='container-xxl'>
           <div className='row'>
-            <div className='col-6'></div>
-            <div className='col-6'></div>
+            <div className='col-6'>
+              <div className='main-product-image'>
+                <div>
+                <ReactImageZoom {...props} />
+                </div>
+              </div>
+              <div className='other-product-images d-flex flex-wrap gap-15'>
+<div> <img src='https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/63/196649/1.jpg?9259' className='img-fluid'/></div>
+<div><img src='https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/63/196649/1.jpg?9259' className='img-fluid'/></div>
+<div><img src='https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/63/196649/1.jpg?9259' className='img-fluid'  /></div>
+<div><img src='https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/63/196649/1.jpg?9259' className='img-fluid'/></div>
+
+              </div>
+            </div>
+            <div className='col-6'>
+              <div className='main-product-details'>
+             <div className='border-bottom'>
+             <h3 className='title'>  Heavy Duty Powerful Blender-2L-Single Cup Black/Red</h3>
+             </div>
+             <div className='border-bottom'>
+              <p className='price'>Ksh 4999</p>
+              <div className='d-flex align-items-center gap-10'>
+              <ReactStars
+count={5}
+size={24} value="4" edit={false}
+activeColor="#ffd700"
+/>
+<p className='mb-0'> 6 Reviews </p>
+              </div>
+             </div>
+              </div>
+            </div>
           </div>
         </div>
        </div>
@@ -22,8 +54,9 @@ const SingleProduct = () => {
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
+            <h4>Product description</h4>
         <div className='bg-white p-3'>
-        <h4>Product description</h4>
+        
           <p className=''>
           Heavy Duty Powerful Blender-2L-Single Cup Black/Red   Need a powerful blender without compromising precious kitchen worktop space?     
 Heavy duty blender is a must-have for every home. 
@@ -39,6 +72,7 @@ This blender will make cooking meals an absolute fun!   You can easily prepare a
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
+              <h3>Reviews</h3>
 <div className='review-inner-wrapper'>
 <div className='review-head d-flex justify-content-between align-items-end'>
  <div>
@@ -60,6 +94,41 @@ activeColor="#ffd700"
 
   )
  }
+</div>
+<div className='review-form py-4 '>
+  <h4> Write a Review</h4>
+<form action="" className="d-flex flex-column gap-15">
+                   <div>
+                   <ReactStars
+count={5}
+size={24} value="4" edit={true}
+activeColor="#ffd700"
+/> 
+                   </div>
+                    <div>
+                <textarea name=""id="" className="w-100 form-control" cols="38" rows="4" placeholder="comments">
+
+                </textarea>
+                    </div>
+                    <div className='d-flex justify-content-end'>
+
+                <button className="button"> Submit Review</button>
+                </div>
+                </form>
+</div>
+<div className='reviews mt-4'>
+  <div className='review'>
+  <div className='d-flex gap-10 align-items-center'>
+    <h6 className='mb-0'>Blender</h6>
+    <ReactStars
+count={5}
+size={24} value="4" edit={false}
+activeColor="#ffd700"
+/>
+  </div>
+<p className='mt-3'>It is working perfectly well hope it will not dissapoint</p> 
+
+  </div>
 </div>
 </div>
             </div>
